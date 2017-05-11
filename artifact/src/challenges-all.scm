e3b10c9eec8edc4416b0833ea2d470251aa3f207
gregr <greg.weiqi@gmail.com>
2017-05-10 22:19:33 -0400

    Add challenges-all.scm

---
+++ artifact/src/challenges-all.scm
@@ -0,0 +1,10 @@
+;; For your safety, this does not run: challenge-2-extra-slow.scm
+
+(printf "Running Challenge 1\n")
+(load "challenge-1.scm")
+
+(printf "Running Challenge 2\n")
+(load "challenge-2.scm")
+
+(printf "Running Challenge 3\n")
+(load "challenge-3.scm")
