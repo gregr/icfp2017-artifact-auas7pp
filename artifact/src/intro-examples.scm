95bce0f16e7c0c6497473604ce88d49e07d042dc
gregr <greg.weiqi@gmail.com>
2017-05-10 22:41:50 -0400

    Add intro-examples.scm

---
+++ artifact/src/intro-examples.scm
@@ -0,0 +1,35 @@
+(load "mk/test-check.scm")
+(load "mk/mk.scm")
+
+(test 'unification
+  (run* (q)
+    (fresh (x y)
+      (== (cons 3 4) (cons x y))
+      ;; All of these expressions are equivalent.
+      (== q (quasiquote (3 ((unquote x) . y) 4)))
+      (== q `(3 (,x . y) 4))
+      (== q (list 3 (cons x (quote y)) 4))
+      (== q (cons 3 (cons (cons x 'y) (cons 4 '()))))))
+  '(((3 (3 . y) 4))))
+
+(define appendo
+  (lambda (l s out)
+    (conde
+      [(== '() l) (== s out)]
+      [(fresh (a d res)
+         (== `(,a . ,d) l)
+         (== `(,a . ,res) out)
+         (appendo d s res))])))
+
+(test 'appendo-forward
+  (run* (q) (appendo '(a b c) '(d e) q))
+  '(((a b c d e))))
+
+(test 'appendo-backward
+  (run* (x y) (appendo x y '(a b c d e)))
+  '(((() (a b c d e)))
+    (((a) (b c d e)))
+    (((a b) (c d e)))
+    (((a b c) (d e)))
+    (((a b c d) (e)))
+    (((a b c d e) ()))))
