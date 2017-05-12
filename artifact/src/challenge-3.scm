25e527e5ca8b87a1bf8be29fe8c24b35b890dd8b
gregr <greg.weiqi@gmail.com>
2017-05-11 20:45:41 -0400

    Write challenge-3 expressions to file

--- artifact/src/challenge-3.scm
+++ artifact/src/challenge-3.scm
@@ -13,8 +13,17 @@
       137)
      (=/= ((_.0 _.1)) ((_.0 lambda))) (num _.2) (sym _.0 _.1))))
 
-(printf "Challenge 3: generate 100 expressions that evaluate to 42 under lexical scope, and 137 under dynamic scope.\n")
-(time
-  (run 100 (expr)
-    (eval-dyno expr 137)
-    (eval-lexo expr 42)))
+(define file-name "challenge-3-generated-expressions.scm")
+(if (file-exists? file-name)
+  (printf "Delete ~s if you'd like to regenerate it.\n" file-name)
+  (begin
+    (printf "Generating 100 expressions that evaluate to 42 under lexical scope, and 137 under dynamic scope.\n")
+    (time
+      (with-output-to-file
+        file-name
+        (lambda ()
+          (pretty-print
+            (run 100 (expr)
+              (eval-dyno expr 137)
+              (eval-lexo expr 42))))))
+    (printf "Generated expressions written to: ~s\n" file-name)))
